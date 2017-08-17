2e914f48d8c5b1dac0ac02e41ff73c47516f4cdb
keros <keroslee@gmail.com>
2017-08-18 02:53:29 +0800

    1.add curl files 2.add script to create shortcut 3.add script to install apt 4.add script to install packages 5.add script to configure ssh config file

--- Cygwin/customize.sh
+++ Cygwin/customize.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-chere -if -t mintty -o "-i /Cygwin-Terminal.ico"
-regtool -w -s set /HKCR/Directory/Background/shell/cygwin64_bash/Icon "$(cygpath -w "$(pwd)")\\Cygwin-Terminal.ico"
+
+#chere -if -t mintty -o "-i /Cygwin-Terminal.ico"
+#regtool -w -s set /HKCR/Directory/Background/shell/cygwin64_bash/Icon "$(cygpath -w "$(pwd)")\\Cygwin-Terminal.ico"
+
+curl https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg > apt && install apt /bin
+rm -f apt
+apt install git zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
--- Cygwin/setup.bat
+++ Cygwin/setup.bat
@@ -1,39 +1,19 @@
 @echo off
-set installFileName=cygwin-setup-x86_64.exe
+set installFileName=setup-x86_64.exe
 set installDir=%~dp0
-echo	' Set your settings								>	download.vbs
-echo	    strFileURL = "https://cygwin.com/setup-x86_64.exe"				>>	download.vbs
-echo	    strHDLocation = "%installFileName%"						>>	download.vbs
 
-echo	' Fetch the file								>>	download.vbs
-echo	    Set objXMLHTTP = CreateObject("MSXML2.XMLHTTP")				>>	download.vbs
+cd %installDir%
+curl https://cygwin.com/setup-x86_64.exe > %installFileName%
+cd ..
 
-echo	    objXMLHTTP.open "GET", strFileURL, false					>>	download.vbs
-echo	    objXMLHTTP.send()								>>	download.vbs
+%installDir%/%installFileName% -qnNdO -R %cd% -s http://cygwin.mirror.constant.com -l %cd% -P curl -P wget
 
-echo	If objXMLHTTP.Status = 200 Then							>>	download.vbs
-echo	Set objADOStream = CreateObject("ADODB.Stream")					>>	download.vbs
-echo	objADOStream.Open								>>	download.vbs
-echo	objADOStream.Type = 1 'adTypeBinary						>>	download.vbs
-
-echo	objADOStream.Write objXMLHTTP.ResponseBody					>>	download.vbs
-echo	objADOStream.Position = 0    'Set the stream position to the start		>>	download.vbs
-
-echo	Set objFSO = Createobject("Scripting.FileSystemObject")				>>	download.vbs
-echo	If objFSO.Fileexists(strHDLocation) Then objFSO.DeleteFile strHDLocation	>>	download.vbs
-echo	Set objFSO = Nothing								>>	download.vbs
-
-echo	objADOStream.SaveToFile strHDLocation						>>	download.vbs
-echo	objADOStream.Close								>>	download.vbs
-echo	Set objADOStream = Nothing							>>	download.vbs
-echo	End if										>>	download.vbs
-
-echo	Set objXMLHTTP = Nothing							>>	download.vbs
-
-download.vbs
-
-%installFileName% -qnNdO -R %installDir% -s http://cygwin.mirror.constant.com -l %installDir% ^
--P git -P wget -P curl -P chere
-
-del download.vbs
-del %installFileName%
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%cd%\Cygwin64 Terminal.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%cd%\bin\mintty.exe" >> CreateShortcut.vbs
+echo oLink.Arguments = " -i /Cygwin-Terminal.ico -" >> CreateShortcut.vbs
+echo oLink.IconLocation ="%cd%\Cygwin-Terminal.ico" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+CreateShortcut.vbs
+del CreateShortcut.vbs
