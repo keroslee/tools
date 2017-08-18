e0ef1a70a6a71a3562e3c7507baf690ff9e1b164
keros <keroslee@gmail.com>
2017-08-18 17:38:13 +0800

    rollback download script

--- Cygwin/setup.bat
+++ Cygwin/setup.bat
@@ -4,8 +4,44 @@ set installDir=%~dp0
 
 cd %installDir%
 curl https://cygwin.com/setup-x86_64.exe > %installFileName%
+
+if exist "%installDir%%installFileName%" goto install
+
+echo	' Set your settings								>	download.vbs
+echo	    strFileURL = "https://cygwin.com/setup-x86_64.exe"				>>	download.vbs
+echo	    strHDLocation = "%installFileName%"						>>	download.vbs
+
+echo	' Fetch the file								>>	download.vbs
+echo	    Set objXMLHTTP = CreateObject("MSXML2.XMLHTTP")				>>	download.vbs
+
+echo	    objXMLHTTP.open "GET", strFileURL, false					>>	download.vbs
+echo	    objXMLHTTP.send()								>>	download.vbs
+
+echo	If objXMLHTTP.Status = 200 Then							>>	download.vbs
+echo	Set objADOStream = CreateObject("ADODB.Stream")					>>	download.vbs
+echo	objADOStream.Open								>>	download.vbs
+echo	objADOStream.Type = 1 'adTypeBinary						>>	download.vbs
+
+echo	objADOStream.Write objXMLHTTP.ResponseBody					>>	download.vbs
+echo	objADOStream.Position = 0    'Set the stream position to the start		>>	download.vbs
+
+echo	Set objFSO = Createobject("Scripting.FileSystemObject")				>>	download.vbs
+echo	If objFSO.Fileexists(strHDLocation) Then objFSO.DeleteFile strHDLocation	>>	download.vbs
+echo	Set objFSO = Nothing								>>	download.vbs
+
+echo	objADOStream.SaveToFile strHDLocation						>>	download.vbs
+echo	objADOStream.Close								>>	download.vbs
+echo	Set objADOStream = Nothing							>>	download.vbs
+echo	End if										>>	download.vbs
+
+echo	Set objXMLHTTP = Nothing							>>	download.vbs
+
+download.vbs
+
 cd ..
 
+:install
+echo installing
 %installDir%/%installFileName% -qnNdO -R %cd% -s http://cygwin.mirror.constant.com -l %cd% -P curl -P wget
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
